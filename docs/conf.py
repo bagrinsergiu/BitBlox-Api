627661b0a237f450c1d2ab3dbf900c97effd9d63
Sergiu Bagrin <bagrin.sergiu@gmail.com>
2016-06-20 12:25:02 +0300

    ...

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'themefuse'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -106,7 +106,7 @@ html_theme = 'themefuse'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["_themes"]
+# html_theme_path = ["_themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -256,3 +256,11 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+# -- Additional options --------------------------------------------------------
+
+# todo_include_todos = True
+
+def setup(app):
+    app.add_stylesheet('_themes/bitblox/static/noun.css')
